497e2eb4eb4ab42a9b4634be3a1365136213438a
Andy Newhouse <andy@techenby.com>
2026-04-06 12:27:54 -0500

    Add back button

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,12 +4,16 @@
         @include('layouts.partials.head')
     </head>
     <body class="min-h-screen">
-        <main class="nativephp-safe-area bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 pt-12">
-            <div class="flex flex-col gap-2 w-full">
+        <main class="nativephp-safe-area flex min-h-svh flex-col gap-6 p-6 pt-12">
+            <div class="flex flex-col gap-2 w-full relative">
                 <div class="flex items-center justify-center gap-2 font-medium mb-6">
                     <flux:avatar :src="asset('icon.png')" />
                     <flux:heading size="xl">{{ config('app.name', 'Laravel') }}</flux:heading>
                 </div>
+
+                @unless (Route::is('splash'))
+                <flux:button :href="route('splash')" icon="arrow-left" class="absolute! top-0 left-0" />
+                @endunless
 
                 {{ $slot }}
             </div>
