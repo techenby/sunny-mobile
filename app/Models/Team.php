113d26e0593f8ba2c5a8e57715ea2a6d52952f51
Andy Newhouse <andy@techenby.com>
2026-04-10 11:51:10 -0500

    Just Keep Swimming 🐠

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Database\Factories\TeamFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Team extends Model
@@ -14,6 +15,18 @@ class Team extends Model
     /** @use HasFactory<TeamFactory> */
     use HasFactory;
     use SoftDeletes;
+
+    /** @return HasMany<Item, $this> */
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    /** @return HasMany<Recipe, $this> */
+    public function recipes(): HasMany
+    {
+        return $this->hasMany(Recipe::class);
+    }
 
     /** @return array<string, string> */
     protected function casts(): array
